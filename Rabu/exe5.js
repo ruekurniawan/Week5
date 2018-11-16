6835275f4a683a7f93915811037da9f050b81fc7
ruekurniawan <ruekurniawan>
2018-11-16 14:56:54 +0700

    update Tugas Dagame

--- Rabu/exe5.js
+++ Rabu/exe5.js
@@ -1,12 +1,14 @@
 function attack (damage) {
-  var serang = damage - 2;
-  return serang;
+  return damage - 2;
 }
 
 function damageCalculation (numberOfAttacks, damagePerAttack) {
-  var simulasiAttack = attack(damagePerAttack);
-  var hitungDamage = simulasiAttack * numberOfAttacks;
-  return hitungDamage;
+  if(numberOfAttacks === 0)
+  {
+  	return 0
+  }
+  else
+  	return attack(damagePerAttack) + damageCalculation(numberOfAttacks-1, damagePerAttack)
 }
 
 // TEST CASE
@@ -15,3 +17,20 @@ console.log(damageCalculation(9, 25)); // 207
 console.log(damageCalculation(10, 4)); // 20
 
 console.log(damageCalculation(5, 20)); // 90
+
+// function attack (damage) {
+//   return damage - 2;
+// }
+
+// function damageCalculation (numberOfAttacks, damagePerAttack) {
+//   var simulasiAttack = attack(damagePerAttack);
+//   var hitungDamage = simulasiAttack * numberOfAttacks;
+//   return hitungDamage;
+// }
+
+// // TEST CASE
+// console.log(damageCalculation(9, 25)); // 207
+
+// console.log(damageCalculation(10, 4)); // 20
+
+// console.log(damageCalculation(5, 20)); // 90
